960687a6a06e22558928d153f5fc9c2d8c924ac2
Anton Tiurin <noxiouz@yandex.ru>
2015-11-17 17:34:51 +0300

    [Main] Add cmdline flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,72 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
 	"net/http"
-	"os"
+	"strings"
 
-	"github.com/gorilla/handlers"
+	log "github.com/Sirupsen/logrus"
+	logformatter "github.com/noxiouz/Combaine/common/formatter"
 
 	"github.com/noxiouz/stout/server"
+	"github.com/noxiouz/stout/version"
 )
 
+type logLevelFlag log.Level
+
+func (l *logLevelFlag) Set(val string) error {
+	level, err := log.ParseLevel(strings.ToLower(val))
+	if err != nil {
+		return err
+	}
+	(*l) = logLevelFlag(level)
+	return nil
+}
+
+func (l *logLevelFlag) TologLevel() log.Level {
+	return log.Level(*l)
+}
+
+func (l *logLevelFlag) String() string {
+	return l.TologLevel().String()
+}
+
+var (
+	httpFlag    string
+	showVersion bool
+
+	loglevel = logLevelFlag(log.DebugLevel)
+)
+
+func init() {
+	flag.StringVar(&httpFlag, "http", ":5432", "endpoint to serve http on")
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.Var(&loglevel, "loglevel", "debug|info|warn|warning|error|panic")
+	flag.Parse()
+
+	log.SetFormatter(&logformatter.CombaineFormatter{})
+	log.SetLevel(loglevel.TologLevel())
+}
+
 func main() {
+	if showVersion {
+		fmt.Printf("version: `%s`\n", version.Version)
+		return
+	}
+
 	isolateServer, err := server.NewIsolateServer()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// log.Fatal(http.ListenAndServe(":5432", isolateServer.Router))
 	server := http.Server{
-		Addr:     ":5432",
-		Handler:  handlers.CombinedLoggingHandler(os.Stderr, isolateServer.Router),
-		ErrorLog: nil,
+		Addr:    httpFlag,
+		Handler: isolateServer.Router,
 	}
-	log.Fatal(server.ListenAndServe())
+
+	log.WithField("endpoint", httpFlag).Info("Start cocaine-porto")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
